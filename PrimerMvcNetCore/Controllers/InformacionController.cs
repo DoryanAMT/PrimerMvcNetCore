5519a31b533de7eaec1872aa6247611aff9df94f
DoryanAlexandro <doryanamt@gmail.com>
2025-01-27 11:50:54 +0100

    Add project files.

---
+++ PrimerMvcNetCore/Controllers/InformacionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerMvcNetCore.Models;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ControladorVista()
+        {
+            //  VAMOS A ENVIAR INFOMRACION SIMPLE A NUESTRA VISTA
+            ViewData["NOMBRE"] = "Alumno";
+            ViewData["EDAD"] = 24;
+            ViewBag.DiaSemana = "Lunes";
+            return View();
+        }
+        public IActionResult ControladorVistaModel()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Alumno";
+            persona.Email = "alumno@email.com";
+            persona.Edad = 27;
+            return View( persona );
+        }
+    }
+}
