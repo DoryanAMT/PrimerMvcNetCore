8239cc6a0714ac59a52a3370cc19df09c5961d99
DoryanAlexandro <doryanamt@gmail.com>
2025-01-28 14:13:43 +0100

    vistas tipo get y post mvc

--- PrimerMvcNetCore/Controllers/InformacionController.cs
+++ PrimerMvcNetCore/Controllers/InformacionController.cs
@@ -9,6 +9,25 @@ namespace PrimerMvcNetCore.Controllers
         {
             return View();
         }
+
+        //  VAMOSA RECIBIR DOS PARAMETROS
+        public IActionResult VistaControllerGet
+            (string saludo, /*System.Nullable<int>*/int? year)
+        {
+            if (year != null)
+            {
+                ViewData["DATA"] = "Hola " + saludo + " en el año " + year;
+            }
+            else
+            {
+                ViewData["DATA"] = "Aquí no tenemos tiempo";
+            }
+            //  PARA COMPROBAR QUE LOS HEMOS RECIBIDO,
+            //  GUARDAMOS LOS PARAMETROS EN ViewData
+            //  PAR VER LA INFORMACION
+            //ViewData["DATA"] = "Hola " + saludo + " en el año " + year;
+            return View();
+        }
         public IActionResult ControladorVista()
         {
             //  VAMOS A ENVIAR INFOMRACION SIMPLE A NUESTRA VISTA
@@ -24,6 +43,26 @@ namespace PrimerMvcNetCore.Controllers
             persona.Email = "alumno@email.com";
             persona.Edad = 27;
             return View( persona );
+        }
+        //  POST RECUPERA NAME COMO REFERENCIA DEL INPUT
+        //  CAPTURAMOS LA VISTA DE VistaControllerPost
+        [HttpPost]
+        public IActionResult VistaControllerPost
+            //(string nombre, string email, int edad)
+            (Persona persona, string aficiones)
+        {
+            ViewData["DATA"] = 
+                "Nombre: " + persona.Nombre
+                + ", Email: " + persona.Email
+                + ", Edad: " + persona.Edad
+                + ", Aficiones: " + aficiones;
+            return View();
+        }
+        //  PARA PODER VER LA VISTA DE VistaControllerPost TAMBIEN NECESITAMOS UN POST
+        [HttpGet]
+        public IActionResult VistaControllerPost()
+        {
+            return View();
         }
     }
 }
---
+++ PrimerMvcNetCore/Controllers/MatematicasController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class MatematicasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //-------------------------------- Sumar Numeros Get -----------------------------
+        public IActionResult SumarNumerosGet
+            (int numero1, int numero2)
+        {
+            ViewData["DATA"] = "La suma de " + numero1 + " + " + numero2 + " = " + (numero1+numero2);
+            return View();
+        }
+        //-------------------------------- Sumar Numeros Post -----------------------------
+        [HttpGet]
+        public IActionResult SumarNumerosPost()
+        {
+            ViewData["NUMERO1"] = 4;
+            ViewData["NUMERO2"] = 6;
+            return View();
+            
+        }
+        [HttpPost]
+        public IActionResult SumarNumerosPost
+            (int numero1, int numero2)
+        {
+            int suma = numero1 + numero2;
+            ViewData["DATA"] = "La suma de " + numero1 + " + " + numero2 + " = " + suma;
+            return View();
+        }
+
+        //-------------------------------- CojeturaCollatz -----------------------------
+        [HttpGet]
+        public IActionResult CojeturaCollatz()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CojeturaCollatz
+            (int numero)
+        {
+            //  DEBEMOS DEVOLVER UN OBJETO COMPLEJO CON
+            //  UNA LISTA DE NUMEROS
+            List<int> numeros = new List<int>();
+            while (numero != 1)
+            {
+                if (numero % 2 == 0)
+                {
+                    numero = numero / 2;
+                }
+                else
+                {
+                    numero = numero * 3 + 1;
+                }
+                numeros.Add(numero);
+            }
+            //  DEVOLVER EL MODEL A LA VISTA
+            return View(numeros);
+        }
+        //-------------------------------- Tabla Multiplicar Simple -----------------------------
+        [HttpGet]
+        public IActionResult TablaMultiplicarSimple()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult TablaMultiplicarSimple
+            (int numero)
+        {
+            List<int> resultados = new List<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = numero * i;
+                resultados.Add(resultado);
+            }
+            return View(resultados);
+        }
+    }
+}
